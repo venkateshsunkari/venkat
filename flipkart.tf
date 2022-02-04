659565d65685a6edb0277245d519dd8c9ff63804
root <root@ip-71-0-1-216.ap-south-1.compute.internal>
2022-02-04 11:18:01 +0000

    second commit

--- flipkart.tf
+++ flipkart.tf
@@ -7,3 +7,28 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+
+
+
+ Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, in case you don't want to create it automatically
+  monitoring_interval = "30"
+  monitoring_role_name = "MyRDSMonitoringRole"
+  create_monitoring_role = true
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  # DB subnet group
+  subnet_ids = ["subnet-12345678", "subnet-87654321"]
+
+  # DB parameter group
+  family = "mysql5.7"
+
+  # DB option group
+  major_engine_version = "5.7"
+
+
+
